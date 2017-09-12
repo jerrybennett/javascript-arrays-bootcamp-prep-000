6300247b8a923fb0e2dfa944f87848ca278eb282
Jerry Bennett <jerrysbusiness@gmail.com>
2017-09-12 01:14:20 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -38,7 +38,8 @@ function removeElementFromBeginningOfArray(array) {
 }
 
 function destructivelyRemoveElementFromEndOfArray(array) {
-  
+  array.pop();
+  return array;
 }
 
 function removeElementFromEndOfArray(array) {
